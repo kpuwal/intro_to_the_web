aebbebab47e52a933612929bddfb399f2f9e0635
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-06-07 12:50:25 +0100

    step 13; added a form

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @name= ["Alex", "Joe", "Matt"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
